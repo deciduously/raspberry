da89bd75a411a6268ab603fd90b1fab67667f8ad
ben <bendlovy@gmail.com>
2016-03-28 22:52:54 -0400

    add model

--- landing/models.py
+++ landing/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class App(models.Model):
+	title = models.CharField(max_length=200)
+	latest_version = models.IntegerField()
+	synopsis = models.TextField(blank=True, null=True)
+	start_date = models.DateTimeField('date initiated')
+	last_update = models.DateTimeField('date updated')
