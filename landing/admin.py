6087fe93f8880a84ea26b00bd434c071b8795728
ben <bendlovy@gmail.com>
2016-04-03 01:28:11 -0400

    Re-separate dice app, revamp landing page

--- landing/admin.py
+++ landing/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import App
+from .models import App, Comment, Post
 
 admin.site.register(App)
+admin.site.register(Comment)
+admin.site.register(Post)
--- landing/views.py
+++ landing/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 from django.template import loader
 
-from .models import App
+from .models import App, Post
 
 # Create your views here.
 def index(request):
+	posts = Post.objects.all()
+	context = {'posts': posts}
 	return render(request, 'landing/index.html')
-
-def dice(request):
-	return render(request, 'landing/dice.html')
 
 def list(request):
 	apps = App.objects.all()
